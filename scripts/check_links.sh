5ee8e365bcaf626efd6782be8bd42a32ac802a07
Evgeny Sinelnikov <sin@altlinux.org>
2022-12-13 04:30:25 +0400

    Update simple script to check if any links are broken

--- scripts/check_links.sh
+++ scripts/check_links.sh
@@ -5,8 +5,23 @@
 set -eu
 
 check_exit_code=0
+grep_option="-e ''"
+verbose=""
 
-git -c grep.lineNumber=false  grep -Iho 'https\?://[[:alnum:]/:._-]*'  | while read link; do
+if [ "$#" -gt 0 ]; then
+    if [ "$1" = "-v" ]; then
+        verbose="1"
+        shift
+    fi
+fi
+
+if [ "$#" -gt 0 ]; then
+    grep_option="$1"
+fi
+
+git -c grep.lineNumber=false  grep -Iho 'https\?://[[:alnum:]/:._-]*' | grep -e "$grep_option" | while read link; do
+#git -c grep.lineNumber=false  grep -Iho 'https\?://[[:alnum:]/:._-]*' | while read link; do
+    test -z "$verbose" || echo "Check link: $link"
     if ! curl -sI -m10 "$link" >/dev/null; then
         echo "Broken link: $link" >&2
         check_exit_code=1
