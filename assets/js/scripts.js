057023dddcd4c423604c1a0b6f9b7bb33aab998f
Maria Alexeeva <alxvmr@altlinux.org>
2025-04-02 12:43:02 +0400

    Add buttons with branch selection

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+function showVersion(version) {
+  var contents = document.querySelectorAll('.links-wrapper[id]');
+  contents.forEach(function(content) {
+    content.style.display = 'none';
+  });
+  document.getElementById(version).style.display = 'block';
+  var buttons = document.querySelectorAll('.switcher button');
+  buttons.forEach(function(button) {
+    button.classList.remove('active');
+  });
+  var activeButton = Array.from(buttons).find(button => button.textContent === version);
+  if (activeButton) {
+    activeButton.classList.add('active');
+  }
+}
+
+function initializePlatforms(platforms) {
+  if (platforms.length > 0) {
+      showVersion(platforms[0]);
+  }
+}
